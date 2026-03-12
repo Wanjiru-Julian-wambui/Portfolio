48d131be1de46795060bdf962c3826c37a1c5052
Julian Wambui <wanjiru.wambui23@students.dkut.ac.ke>
2026-03-12 19:42:35 +0300

    fix uploadcare error

--- app/Http/Resources/SkillResource.php
+++ app/Http/Resources/SkillResource.php
@@ -8,11 +8,14 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class SkillResource extends JsonResource
 {
     public function toArray(Request $request): array
-    {
-        return [
-            'id'    => $this->id,
-            'name'  => $this->name,
-            'image' => 'https://ucarecdn.net/' . $this->image . '/',
-        ];
-    }
+{
+    return [
+        'id'    => $this->id,
+        'name'  => $this->name,
+        // Only build the URL if the UUID exists
+        'image' => $this->image 
+            ? "https://ucarecdn.net/{$this->image}/" 
+            : null,
+    ];
+}
 }
